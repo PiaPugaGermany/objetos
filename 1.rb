1b63a658918cb9b3b839f0457899ce07d22d31bd
piapugagermany <pipuge@gmail.com>
2019-06-07 19:53:53 -0400

    second commit

--- 1.rb
+++ 1.rb
@@ -1,14 +1,17 @@
-# Copia el siguiente código y ejecútalo. Luego, corrige los errores para poder imprimir ejecutar
-# ambos métodos y finalmente obtener el valor de la variable de a.
+# Copia el siguiente código y ejecútalo. Luego, corrige los errores para poder imprimir ejecutar ambos métodos y finalmente obtener el valor de la variable de a.
 
 class Anything
-def foo
-a = 5
+  def foo
+    @a = 5
+    end
+  def bar
+    @a += 1
+  end
+  def a
+    puts @a
+  end
 end
-def bar
-@a += 1
-end
-end
+
 any = Anything.new
 any.foo
 any.bar
