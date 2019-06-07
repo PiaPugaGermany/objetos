7711484a8fa547717d59ddea3d9cb28694eb583d
piapugagermany <pipuge@gmail.com>
2019-06-07 19:34:13 -0400

    first commit

---
+++ 3.rb
@@ -0,0 +1,10 @@
+# Copia el siguiente código y ejecútalo. Luego corrige los errores para poder imprimir el nombre de la
+# tienda.
+
+class Store
+def initialize(name)
+name = @name
+end
+end
+store = store.new('Tienda 1')
+puts store.name
---
+++ 4.rb
@@ -0,0 +1,7 @@
+# Crea una clase llamada Dog cuyo constructor reciba como argumento un hash con la siguiente
+# estructura:
+
+propiedades = {nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café'}
+
+# Instanciar un nuevo perro a partir de las propiedades contenidas en el hash. Luego generar un
+# método llamado ladrar que, mediante interpolación, imprima "Beethoven está ladrando!"
