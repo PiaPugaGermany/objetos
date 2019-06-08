a91d8c75d3aab3efcbc8afe99f97dace367f1b59
piapugagermany <pipuge@gmail.com>
2019-06-07 20:41:39 -0400

    quinto

--- 4.rb
+++ 4.rb
@@ -1,7 +1,21 @@
-# Crea una clase llamada Dog cuyo constructor reciba como argumento un hash con la siguiente
-# estructura:
+# Crea una clase llamada Dog cuyo constructor reciba como argumento un hash con la siguiente estructura:
 
 propiedades = {nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café'}
 
-# Instanciar un nuevo perro a partir de las propiedades contenidas en el hash. Luego generar un
-# método llamado ladrar que, mediante interpolación, imprima "Beethoven está ladrando!"
+class Dog
+attr_accessor
+  def initialize (a)
+    @nombre = a[:nombre]
+    @raza = a[:raza]
+    @color = a[:color]
+  end
+  def ladrar
+    print "#{@nombre} está ladrando!"
+  end
+end
+
+dog = Dog.new(propiedades)
+dog.ladrar
+
+
+# Instanciar un nuevo perro a partir de las propiedades contenidas en el hash. Luego generar un método llamado ladrar que, mediante interpolación, imprima "Beethoven está ladrando!"
